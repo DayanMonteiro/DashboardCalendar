16a0da1ddd8d34ba36999322254d1ffa720ec05c
DayanMonteiro <dmsmamb@gmail.com>
2023-10-09 10:04:52 -0300

    header dashboard component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,18 @@
 import moment from "moment/moment";
-import Sidebar from "../Sidebar/Sidebar";
 import css from "./Layout.module.css";
 import { BiSearch } from "react-icons/bi";
+import Sidebar from "../Sidebar/Sidebar";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const Layout = () => {
+  const { pathname } = useLocation();
+
   return (
     <div className={css.container}>
       <Sidebar />
+
+      {/* making the dashboard as the default route */}
+      {pathname === "/" && <Navigate to="/dashboard" />}
 
       <div className={css.dashboard}>
         <div className={css.topBaseGradients}>
@@ -31,8 +37,13 @@ const Layout = () => {
             </div>
           </div>
         </div>
+
+        <div className={css.content}>
+          <Outlet />
+        </div>
       </div>
     </div>
   );
 };
+
 export default Layout;
