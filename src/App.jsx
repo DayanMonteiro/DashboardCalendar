ccb75b4e0c8b95bdbbcc3c71ece0e0a13218d358
DayanMonteiro <dmsmamb@gmail.com>
2023-10-10 12:41:50 -0300

    trello

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Calendar from "./pages/Calendar/Calendar";
 import Layout from "./components/Layout/Layout";
+import BoardPage from "./pages/Board/Board";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
           <Route path="/" element={<Layout />}>
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="calendar" element={<Calendar />} />
+            <Route path="board" element={<BoardPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
